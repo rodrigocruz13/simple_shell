93fcc35c5cdc8e512c18d59c4dc5ac54ff984d53
Ryan Hudson <ryan.e.hudson@gmail.com>
2019-04-15 23:13:10 +0000

    Fix problem with commands beginning with './'

--- execute_cmd.c
+++ execute_cmd.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
-  * _put_string - prints a string
-  * @str: string to print
-  */
+ * _put_string - prints a string
+ * @str: string to print
+ */
 void _put_string(char *str)
 {
 	int i = 0;
@@ -11,9 +11,9 @@ void _put_string(char *str)
 		_putchar(str[i]);
 }
 /**
-  * _put_error - prints an error message
-  * @tokens: command that is an error
-  */
+ * _put_error - prints an error message
+ * @tokens: command that is an error
+ */
 void _put_error(char **tokens)
 {
 	char *args = HSH_ARGV_ZERO;
@@ -27,12 +27,31 @@ void _put_error(char **tokens)
 	_put_string(nf);
 
 }
+/**
+  * ex_dot_slash - executes commands beginning with a './'
+  * @tokens: commands to execute
+  * @current_path: current path
+  */
+void ex_dot_slash(char **tokens, char *current_path)
+{
+	int j = 0;
+
+	while (tokens[0][j + 1] != '\0')
+	{
+		tokens[0][j] = tokens[0][j + 1];
+		j++;
+	}
+	tokens[0][j] = '\0';
+	_strcat(current_path, tokens[0]);
+	tokens[0] = current_path;
+	execve(tokens[0], tokens, SHELL_ENV_VARS);
+}
 
 /**
-  * execute_cmd - executes commands sent by user
-  * @tokens: commands and arguments to execute
-  * Return: -1 on error
-  */
+ * execute_cmd - executes commands sent by user
+ * @tokens: commands and arguments to execute
+ * Return: -1 on error
+ */
 int execute_cmd(char **tokens)
 {
 	char **path = get_path(getenv("PATH"));
@@ -43,10 +62,14 @@ int execute_cmd(char **tokens)
 	char buffer[100];
 	char *current_path = getcwd(buffer, 100);
 
-
 	if (tokens[0][0] == '/')
 		execve(tokens[0], tokens, SHELL_ENV_VARS);
 
+	if (tokens[0][0] == '.' && tokens[0][1] == '/')
+	{
+		ex_dot_slash(tokens, current_path);
+	}
+	printf("Tokens[0]: %s\n", tokens[0]);
 	while (path[i] != NULL)
 	{
 		chdir(path[i]);
