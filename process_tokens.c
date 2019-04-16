0d1edde4d0646ef42ec60418e31cf449b2faef69
Ryan Hudson <ryan.e.hudson@gmail.com>
2019-04-16 00:23:48 +0000

    Revert "Add env builtin"
    This reverts commit 7ca6075f73f50e4a41c02476ca80daeae3f881e4.

--- process_tokens.c
+++ process_tokens.c
@@ -8,17 +8,12 @@ int process_tokens(char **tokens)
 {
 	int status = 0;
 	char *b_exit = "exit";
-	char *b_env = "env";
 
 	if (tokens[0] == NULL)
 		return (1);
 	if (_strcmp(tokens[0], b_exit) == 0)
 		return (-1);
-	if (_strcmp(tokens[0], b_env) == 0)
-	{
-		put_env();
-		return (1);
-	}
+
 	status = create_fork(tokens);
 
 	return (status);
